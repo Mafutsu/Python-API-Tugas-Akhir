ac6bc55b69aa422215034b26bdd5132a608b0246
Michael <mquantum07@gmail.com>
2026-05-27 12:21:03 +0700

    add related laws

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from bm25_engine import BM25Engine
 from sbert_engine import SBERTSearchEngine
 from fusion import HybridSearchEngine
+import json
 
 app = FastAPI()
 
@@ -17,6 +18,15 @@ app.add_middleware(
 )
 
 DATA_PATH = "UU_Pidana_clean_updated3.json"
+
+with open(DATA_PATH, "r", encoding="utf-8") as f:
+    dataset = json.load(f)
+
+# mapping id -> data
+law_map = {}
+
+for item in dataset:
+    law_map[str(item["id"])] = item
 
 INDEX_PATH = "index/faiss_index.bin"
 
@@ -40,5 +50,37 @@ engine = HybridSearchEngine(
 def search(query: str):
 
     results = engine.search(query)
+
+    for result in results:
+
+        related_laws = []
+
+        hyperlinks = result.get("hyperlink", [])
+
+        if hyperlinks:
+
+            for law_id in hyperlinks:
+
+                law_id = str(law_id)
+
+                if law_id in law_map:
+
+                    related = law_map[law_id]
+
+                    related_laws.append({
+
+                        "nomor": related.get("nomor"),
+
+                        "tahun": related.get("tahun"),
+
+                        "pasal": related.get("pasal"),
+
+                        "ayat": related.get("ayat"),
+
+                        "content": related.get("content")
+
+                    })
+
+        result["related_laws"] = related_laws
 
     return results
